30b097e86ddd633fe6b5e80180df7112661ee7b8
lyj <leeyujin95y@naver.com>
2020-07-19 02:32:06 +0900

    [ daily_routine ] 스프링 스터디 시작

---
+++ src/test/java/com/daily_routine/DailyRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily_routine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
