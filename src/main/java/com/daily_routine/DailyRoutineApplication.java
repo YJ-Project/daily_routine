c8b3817b7fabf14236ec236a4c1a91db256a3949
lyj <leeyujin95y@naver.com>
2020-08-08 16:53:00 +0900

    log4j, org.mariadb.jdbc 설정 추가

--- src/main/java/com/daily_routine/DailyRoutineApplication.java
+++ src/main/java/com/daily_routine/DailyRoutineApplication.java
@@ -1,12 +1,18 @@
 package com.daily_routine;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DailyRoutineApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(DailyRoutineApplication.class);
+
 
 	public static void main(String[] args) {
+		log.info("로그 ㅎㅇ");
 		SpringApplication.run(DailyRoutineApplication.class, args);
 	}
 
